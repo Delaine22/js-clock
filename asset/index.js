689ceaef025150829d452563d05c06f2adbe997b
Delaine22 <108828746+Delaine22@users.noreply.github.com>
2023-07-24 14:21:23 +0100

    responsividade

--- asset/index.js
+++ asset/index.js
@@ -1,22 +1,18 @@
 const hour = document.getElementById('hora')
 const minute = document.getElementById('minuto')
 const second = document.getElementById('segundo')
-const milesim = document.getElementById('milesimo')
 
 const container = setInterval(function tempo(){
 var hoje = new Date()
 var hr = hoje.getHours()
 var min = hoje.getMinutes()
 var sec = hoje.getSeconds()
-var mil = hoje.getMilliseconds()
 
 if (hr < 10) hr = '0'+ hr
 if(min <10) min = '0'+ min
 if (sec <10) sec ='0'+ sec
-if (mil <10) mil = '0'+ mil
 
 hour.innerHTML = hr
 minute.innerHTML = min
 second.innerHTML = sec
-milesim.innerHTML = mil
 })
